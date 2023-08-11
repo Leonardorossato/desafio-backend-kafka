a1c82a048d587aad22bbb445b1a2043f41a1870b
Leonardorossato <leonardorossato21@gmail.com>
2023-08-11 13:36:33 -0300

    importação dos arquivos criados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CustomerModule } from './customer/customer.module';
 import { ConfigModule } from '@nestjs/config';
+import { ItemsModule } from './items/items.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [CustomerModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [CustomerModule, ConfigModule.forRoot({ isGlobal: true }), ItemsModule, CartModule],
 })
 export class AppModule {}
