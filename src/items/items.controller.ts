9d054212f22dbccd4c19bcc7e9583ac898b02d74
Leonardorossato <leonardorossato21@gmail.com>
2023-08-11 13:52:57 -0300

    limpeza

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,34 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ItemsService } from './items.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
-import { UpdateItemDto } from './dto/update-item.dto';
+import { ItemsService } from './items.service';
 
 @Controller('items')
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
 
-  @Post()
-  create(@Body() createItemDto: CreateItemDto) {
-    return this.itemsService.create(createItemDto);
+  @Post('/create')
+  async create(@Body() dto: CreateItemDto) {
+    return await this.itemsService.create(dto);
   }
 
   @Get()
   findAll() {
     return this.itemsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.itemsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateItemDto: UpdateItemDto) {
-    return this.itemsService.update(+id, updateItemDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.itemsService.remove(+id);
   }
 }
