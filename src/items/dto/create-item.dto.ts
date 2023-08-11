f930c3499333dd6fe68160c70dce32e6de2c9b6e
Leonardorossato <leonardorossato21@gmail.com>
2023-08-11 13:52:53 -0300

    update create-item.dto.ts

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,1 +1,9 @@
-export class CreateItemDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateItemDto {
+  @ApiProperty({ nullable: false })
+  name: string;
+
+  @ApiProperty({ nullable: false })
+  quantity: number;
+}
