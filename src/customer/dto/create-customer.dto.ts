89d1eb42138daed5a61899ba0bb898e85b52b4bc
Leonardorossato <leonardorossato21@gmail.com>
2023-08-11 13:04:29 -0300

    import swagger

--- src/customer/dto/create-customer.dto.ts
+++ src/customer/dto/create-customer.dto.ts
@@ -1,1 +1,21 @@
-export class CreateCustomerDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateCustomerDto {
+  @ApiProperty({ nullable: false })
+  @IsNotEmpty()
+  firstName: string;
+
+  @ApiProperty({ nullable: false })
+  @IsNotEmpty()
+  lastName: string;
+
+  @ApiProperty({ nullable: false })
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({ nullable: false })
+  @IsNotEmpty()
+  phone: string;
+}
