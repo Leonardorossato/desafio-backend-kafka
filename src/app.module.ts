d58770d3519d654dff46b2c15a55703091a379c3
Leonardorossato <leonardorossato21@gmail.com>
2023-08-11 11:11:43 -0300

    configuração env global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CustomerModule } from './customer/customer.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CustomerModule],
+  imports: [CustomerModule, ConfigModule.forRoot({ isGlobal: true })],
 })
 export class AppModule {}
