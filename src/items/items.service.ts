b18299ed3e6b804aee6263da155b8d64d932e3cb
Leonardorossato <leonardorossato21@gmail.com>
2023-08-11 13:52:37 -0300

    serviço para o item criado

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,26 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateItemDto } from './dto/create-item.dto';
-import { UpdateItemDto } from './dto/update-item.dto';
+import { Item } from './entities/item.entity';
 
 @Injectable()
 export class ItemsService {
-  create(createItemDto: CreateItemDto) {
-    return 'This action adds a new item';
+  constructor(
+    @InjectRepository(Item) private readonly itemRepository: Repository<Item>,
+  ) {}
+  async create(dto: CreateItemDto) {
+    try {
+      const item = await this.itemRepository.create(dto);
+      await this.itemRepository.save(item);
+      return item;
+    } catch (error) {
+      throw new HttpException(
+        'Erro ao criar este item',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 
   findAll() {
     return `This action returns all items`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} item`;
-  }
-
-  update(id: number, updateItemDto: UpdateItemDto) {
-    return `This action updates a #${id} item`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} item`;
   }
 }
