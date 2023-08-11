2d0b8518d23507830258d4054053ca66e6f4abb5
Leonardorossato <leonardorossato21@gmail.com>
2023-08-11 14:17:12 -0300

    entidade carrinho

--- src/cart/entities/cart.entity.ts
+++ src/cart/entities/cart.entity.ts
@@ -1,1 +1,15 @@
-export class Cart {}
+import { Customer } from 'src/customer/entities/customer.entity';
+import { Item } from 'src/items/entities/item.entity';
+import { Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Cart {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Item, (items) => items.id)
+  items: Item[];
+
+  @ManyToOne(() => Customer, (customer) => customer.id)
+  customer: Customer;
+}
