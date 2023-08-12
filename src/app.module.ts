1234f9cd2526d0d90f294f2b6ba874eab666c0d0
Leonardorossato <leonardorossato21@gmail.com>
2023-08-12 09:51:46 -0300

    limpeza das imortações não utilizadas mais

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { CustomerModule } from './customer/customer.module';
 import { ConfigModule } from '@nestjs/config';
-import { ItemsModule } from './items/items.module';
 import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [CustomerModule, ConfigModule.forRoot({ isGlobal: true }), ItemsModule, CartModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), CartModule],
 })
 export class AppModule {}
