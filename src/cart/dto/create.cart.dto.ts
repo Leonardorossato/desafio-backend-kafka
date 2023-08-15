5eec9d6ec43b13cfdee8a21aafc376d248eb9b26
Leonardorossato <leonardorossato21@gmail.com>
2023-08-15 14:46:52 -0300

    update create.cart.dto.ts

--- src/cart/dto/create.cart.dto.ts
+++ src/cart/dto/create.cart.dto.ts
@@ -1,1 +1,14 @@
-export class CreateCartDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Customer } from '../interface/customer.interface';
+import { CartItem } from '../interface/item.interface';
+
+export class CreateCartDto {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty({ type: Array })
+  items: CartItem[];
+
+  @ApiProperty({ type: Object })
+  customer: Customer;
+}
