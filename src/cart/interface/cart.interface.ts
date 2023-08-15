8280055e6e9faa2b36e2172d4aafe9e235597dca
Leonardorossato <leonardorossato21@gmail.com>
2023-08-15 14:46:32 -0300

    interfaces atualizadas

--- src/cart/interface/cart.interface.ts
+++ src/cart/interface/cart.interface.ts
@@ -2,7 +2,7 @@ import { Customer } from './customer.interface';
 import { CartItem } from './item.interface';
 
 export interface Cart {
-  id: number;
+  id: string;
   items: CartItem[];
   customer: Customer;
 }
